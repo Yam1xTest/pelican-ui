15895e569a23a0cd9712dbb209fc73efcd75197d
Anna <141008173+Annushkaaaaa@users.noreply.github.com>
2024-10-11 09:03:44 +0000

    chore: change url to cms

--- src/common/utils/getStrapiURL.ts
+++ src/common/utils/getStrapiURL.ts
@@ -1,4 +1,4 @@
 export function getStrapiURL(path = '') {
-  return `${'http://pelican.local.tourmalinecore.internal'}${path}`;
+  return `${'http://pelican-cms-nginx'}${path}`;
   // return `${'http://localhost:1337'}${path}`;
 }
