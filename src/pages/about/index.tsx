8848a1cd26602d389e89211adbf26714d0acab7d
Artem Sheptunov <sheptunov-aa@mail.ru>
2024-11-05 14:38:27 +0500

    feat: add grid template

---
+++ src/pages/about/index.tsx
@@ -0,0 +1,13 @@
+export default function About() {
+  return (
+    <section className="about">
+      <div className="about__item1">1</div>
+      <div className="about__item2">2</div>
+      <div className="about__item3">
+        <div className="about__item4">4</div>
+        <div className="about__item5">5</div>
+      </div>
+      <div className="about__item6">6</div>
+    </section>
+  );
+}
