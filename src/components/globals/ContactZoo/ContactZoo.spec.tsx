81ce3357419cabf20103e41816e952e13ec6a37e
Artem Sheptunov <sheptunov-aa@mail.ru>
2024-11-07 18:19:44 +0500

    test: add ContactZoo component tests

---
+++ src/components/globals/ContactZoo/ContactZoo.spec.tsx
@@ -0,0 +1,56 @@
+import { Breakpoint } from '@/src/common/enum';
+import { setViewportSizeAndGoToPage } from '@/test/helpers';
+import { test, expect, Page } from '@playwright/test';
+
+test.describe(`ContactZooComponentTests`, () => {
+  test(`MobileTest`, mobileTest);
+
+  test(`TabletXlTest`, tabletXlTest);
+
+  test(`DesktopTest`, desktopTest);
+});
+
+async function mobileTest({
+  page,
+}: {
+  page: Page,
+}) {
+  await setViewportSizeAndGoToPage({
+    page,
+  });
+
+  await expect(getContactZooByTestId({ page }))
+    .toHaveScreenshot(`contact-zoo-mobile.png`);
+}
+
+async function tabletXlTest({
+  page,
+}: {
+  page: Page,
+}) {
+  await setViewportSizeAndGoToPage({
+    page,
+    width: Breakpoint.TABLET_XL,
+  });
+
+  await expect(getContactZooByTestId({ page }))
+    .toHaveScreenshot(`contact-zoo-tablet.png`);
+}
+
+async function desktopTest({
+  page,
+}: {
+  page: Page,
+}) {
+  await setViewportSizeAndGoToPage({
+    page,
+    width: Breakpoint.DESKTOP,
+  });
+
+  await expect(getContactZooByTestId({ page }))
+    .toHaveScreenshot(`contact-zoo-desktop.png`);
+}
+
+function getContactZooByTestId({ page }: { page: Page }) {
+  return page.getByTestId(`contact-zoo`);
+}
