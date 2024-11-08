0ea294fa808324ca3964374d7a2744b7a6a2bdc4
Dmitriy Myakotin <75628188+MDI74@users.noreply.github.com>
2024-11-08 12:00:08 +0500

    chore: #9: change inter fonts and fix tests

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,9 @@
-/* eslint-disable @typescript-eslint/quotes */
 import '../styles/index.scss';
 import type { AppProps } from 'next/app';
-import { Inter } from 'next/font/google';
-
-const inter = Inter({
-  weight: ['400', '500', '600'],
-  subsets: ['latin', 'cyrillic'],
-});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div className={inter.className}>
+    <div>
       <Component {...pageProps} />
     </div>
   );
