4076beaf9a5f66f7017c22d1e9404f0e271500fa
Artem Sheptunov <sheptunov-aa@mail.ru>
2024-11-13 15:19:14 +0500

    feat: add Video component

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  webpack(config, { isServer }) {
+    config.module.rules.push({
+      test: /\.(mp4|webm|ogg|swf|ogv)$/,
+      use: [
+        {
+          loader: 'file-loader',
+          options: {
+            publicPath: '/_next/static/videos/',
+            outputPath: 'static/videos/',
+            name: '[name].[hash].[ext]',
+            esModule: false,
+          },
+        },
+      ],
+    });
+
+    return config;
+  },
   reactStrictMode: true,
   output: "standalone",
 
---
+++ src/components/globals/Video/Video.tsx
@@ -0,0 +1,38 @@
+import Plyr from 'plyr-react';
+import { Options, Source, Track } from 'plyr';
+import 'plyr-react/plyr.css';
+
+type VideoProps = {
+  className: string,
+  title?: string,
+  sources: Source[],
+  poster?: string,
+  tracks?: Track[],
+  options?: Options,
+};
+
+export function Video({
+  className,
+  title,
+  sources,
+  poster,
+  tracks,
+  options,
+}: VideoProps) {
+  return (
+    <div
+      className={className}
+    >
+      <Plyr
+        source={{
+          type: `video`,
+          title,
+          sources,
+          poster,
+          tracks,
+        }}
+        options={options}
+      />
+    </div>
+  );
+}
