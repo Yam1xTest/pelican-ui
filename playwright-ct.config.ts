3e10c214f1c571d3aba86273545d4c6a26c155ee
Dmitriy Myakotin <75628188+MDI74@users.noreply.github.com>
2024-11-20 18:28:43 +0500

    ci: update ci

--- playwright-ct.config.ts
+++ playwright-ct.config.ts
@@ -15,11 +15,9 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 4 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: `html`,
+  reporter: process.env.CI ? `blob` : `html`,
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
