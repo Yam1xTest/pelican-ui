b954e7a45eebb67ea980625f80ae9ea8eee50d78
Dmitriy Myakotin <75628188+MDI74@users.noreply.github.com>
2024-11-20 11:47:23 +0500

    fix: fix ts error

--- src/components/globals/Header/Header.tsx
+++ src/components/globals/Header/Header.tsx
@@ -97,11 +97,6 @@ export function Header({
           phone={phone}
           navigationLinks={navigationLinks}
           popupTicketBuyText={popupTicketBuyText}
-          officialLinks={[]}
-          footerUserLinks={[]}
-          footerAboutLinks={[]}
-          footerNavTitleLeft=""
-          footerNavTitleRight=""
         />
       )}
     </div>
