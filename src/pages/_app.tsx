6b319fe25fd84424ce68a39d9afee69396f5fe67
Dmitriy Myakotin <75628188+MDI74@users.noreply.github.com>
2024-11-25 13:42:59 +0500

    chore: move get globalData to _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,18 @@
-/* eslint-disable */
+/* eslint-disable @typescript-eslint/quotes */
 import '../styles/index.scss';
 import type { AppProps } from 'next/app';
-
 import localFont from "next/font/local";
+import {
+  EMAIL,
+  FOOTER_ABOUT_LINKS,
+  FOOTER_NAV_TITLE_LEFT,
+  FOOTER_NAV_TITLE_RIGHT,
+  FOOTER_USER_LINKS,
+  NAVIGATION_LINKS,
+  OFFICIAL_LINKS,
+  PHONE,
+  POPUP_TICKET_BUY_TEXT,
+} from '../common/mocks/globals-mock';
 
 const inter = localFont({
   src: [
@@ -30,10 +40,38 @@ const inter = localFont({
   variable: '--font-inter',
 });
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({
+  Component, pageProps,
+}: AppProps) {
   return (
     <div className={inter.variable}>
       <Component {...pageProps} />
     </div>
   );
 }
+
+App.getInitialProps = async () => ({
+  // TODO Uncomment when the api appears, there will be static data here
+  // if (process.env.APP_ENV === `test`) {
+  //   return {
+  //     props: {
+  //       navigationLinks: NAVIGATION_LINKS,
+  //     },
+  //   };
+  // }
+
+  // TODO there will be a request in the Strapi api here
+  pageProps: {
+    globalData: {
+      popupTicketBuyText: POPUP_TICKET_BUY_TEXT,
+      email: EMAIL,
+      phone: PHONE,
+      navigationLinks: NAVIGATION_LINKS,
+      footerAboutLinks: FOOTER_ABOUT_LINKS,
+      footerUserLinks: FOOTER_USER_LINKS,
+      officialLinks: OFFICIAL_LINKS,
+      footerNavTitleLeft: FOOTER_NAV_TITLE_LEFT,
+      footerNavTitleRight: FOOTER_NAV_TITLE_RIGHT,
+    },
+  },
+});
