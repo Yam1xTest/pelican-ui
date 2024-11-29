8d1f976c74113c2584a514db95b7bf303d21a9e9
ArinaKond <arishakond@mail.ru>
2024-11-29 11:09:41 +0500

    test: #27: add DocumentsCategoriesList tests

---
+++ src/components/documents-page/DocumentsCategories/DocumentsCategoriesList.spec.tsx
@@ -0,0 +1,115 @@
+import { AppRoute, Breakpoint } from '@/src/common/enum';
+import { gotoPage, hideHeader, setViewportSize } from '@/test/helpers';
+import { test, expect, Page } from '@playwright/test';
+
+test.describe(`DocumentsCategoriesListComponentTests`, () => {
+  test.beforeEach(async ({
+    page,
+  }) => {
+    await gotoPage({
+      page,
+      url: AppRoute.DOCUMENTS,
+    });
+
+    await hideHeader({
+      page,
+    });
+  });
+
+  test(`MobileTest`, mobileTest);
+
+  test(`TabletTest`, tabletTest);
+
+  test(`TabletXlTest`, tabletXlTest);
+
+  test(`DesktopTest`, desktopTest);
+
+  test(`DesktopXlTest`, desktopXlTest);
+});
+
+async function mobileTest({
+  page,
+}: {
+  page: Page,
+}) {
+  await setViewportSize({
+    page,
+  });
+
+  await expect(getDocumentsCategoriesListByTestId({
+    page,
+  }))
+    .toHaveScreenshot(`documents-categories-list-mobile.png`);
+}
+
+async function tabletTest({
+  page,
+}: {
+  page: Page,
+}) {
+  await setViewportSize({
+    page,
+    width: Breakpoint.TABLET,
+  });
+
+  await expect(getDocumentsCategoriesListByTestId({
+    page,
+  }))
+    .toHaveScreenshot(`document-categories-list-tablet.png`);
+}
+
+async function tabletXlTest({
+  page,
+}: {
+  page: Page,
+}) {
+  await setViewportSize({
+    page,
+    width: Breakpoint.TABLET_XL,
+  });
+
+  await expect(getDocumentsCategoriesListByTestId({
+    page,
+  }))
+    .toHaveScreenshot(`documents-categories-list-tablet-xl.png`);
+}
+
+async function desktopTest({
+  page,
+}: {
+  page: Page,
+}) {
+  await setViewportSize({
+    page,
+    width: Breakpoint.DESKTOP,
+  });
+
+  await expect(getDocumentsCategoriesListByTestId({
+    page,
+  }))
+    .toHaveScreenshot(`documents-categories-list-desktop.png`);
+}
+
+async function desktopXlTest({
+  page,
+}: {
+  page: Page,
+}) {
+  await setViewportSize({
+    page,
+    width: Breakpoint.DESKTOP_XL,
+  });
+
+  await expect(getDocumentsCategoriesListByTestId({
+    page,
+  }))
+    .toHaveScreenshot(`documents-categories-list-desktop-xl.png`);
+}
+
+function getDocumentsCategoriesListByTestId({
+  page,
+}: {
+  page: Page
+}) {
+  return page.getByTestId(`documents-categories-list`);
+}
