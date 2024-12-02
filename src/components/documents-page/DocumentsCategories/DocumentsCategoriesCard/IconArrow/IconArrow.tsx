3db58473bf29e738205e74dcdc5c8888c971b952
Artem Sheptunov <sheptunov-aa@mail.ru>
2024-12-02 14:54:36 +0500

    style: #27: finish mobile DocumentCategoryCard styles

---
+++ src/components/documents-page/DocumentsCategories/DocumentsCategoriesCard/IconArrow/IconArrow.tsx
@@ -0,0 +1,18 @@
+export function IconArrow() {
+  return (
+    <svg
+      width="12"
+      height="11"
+      viewBox="0 0 12 11"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M1.6 10.1L11 1V11M12 1H1"
+        stroke="#0E6E34"
+        stroke-width="2"
+      />
+    </svg>
+
+  );
+}
