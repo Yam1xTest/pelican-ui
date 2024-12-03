afdff6b2eeeddada02591b33c81b1b9ff6d13e5a
Arina <arishakond@mail.ru>
2024-12-03 16:46:06 +0500

    refactor: #27: remove redundant case

--- src/common/utils/getMockPageData.ts
+++ src/common/utils/getMockPageData.ts
@@ -1,7 +1,6 @@
 import { AppRoute } from "../enum";
 import { HOME_PAGE } from "../mocks/home-page-mock/home-page-mock";
 import { NOT_FOUND_PAGE } from "../mocks/not-found-page-mock/not-found-page-mock";
-import { DOCUMENTS_PAGE } from "../mocks/documents-page-mock/documents-page-mock";
 
 export function getMockPageData({
   slug,
@@ -11,9 +10,6 @@ export function getMockPageData({
   switch (`/${slug}`) {
     case AppRoute.HOME:
       return HOME_PAGE;
-
-    case AppRoute.DOCUMENTS:
-      return DOCUMENTS_PAGE;
 
     default:
       return NOT_FOUND_PAGE;
