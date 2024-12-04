1382e5332436a86c84ba5e361b435b133bd7d198
Artem Sheptunov <sheptunov-aa@mail.ru>
2024-12-04 15:57:31 +0500

    feat: #29: add DocumentsList component markup

---
+++ src/components/documents-page/DocumentsList/DocumentsList.tsx
@@ -0,0 +1,38 @@
+import { DocumentsListComponentProps } from "@/src/common/mocks/documents-page-mock/documents-list-mock";
+import { DocumentCard } from "@/src/components/documents-page/DocumentsList/DocumentCard/DocumentCard";
+
+export function DocumentsList({
+  categoryTitle,
+  documents,
+}: {
+  categoryTitle: string,
+  documents: DocumentsListComponentProps[],
+}) {
+  return (
+    <section className="documents">
+      <h1 className="documents__title">{categoryTitle}</h1>
+      <ul className="documents__list">
+        {documents?.map(({
+          id,
+          date,
+          showDate,
+          title,
+          subtitle,
+          description,
+          files,
+        }) => (
+          <DocumentCard
+            className="documents__item"
+            key={id}
+            date={date}
+            showDate={showDate}
+            title={title}
+            subtitle={subtitle}
+            description={description}
+            files={files}
+          />
+        ))}
+      </ul>
+    </section>
+  );
+}
