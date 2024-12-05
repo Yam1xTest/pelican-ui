91ac24f1b41f85e5e6af2713dcbd9dc00e1c2a81
Ekaterina <echekina@gmail.com>
2024-12-05 16:01:21 +0500

    fix: #62: footer

--- src/components/globals/Footer/Footer.tsx
+++ src/components/globals/Footer/Footer.tsx
@@ -42,126 +42,128 @@ export function Footer({
       className="footer"
       data-testid="footer"
     >
-      <div className="container footer__inner">
-        <div className="footer__top">
-          <div className="footer__cols">
-            <div className="footer__col footer__col--left">
-              <h3 className="footer__title">{footerNavTitleLeft}</h3>
-              <ul className="footer__nav">
-                <li
-                  className="footer__nav-item"
-                  key={popupTicketBuyText}
+      <div className="footer__inner">
+        <div className="container">
+          <div className="footer__top">
+            <div className="footer__cols">
+              <div className="footer__col footer__col--left">
+                <h3 className="footer__title">{footerNavTitleLeft}</h3>
+                <ul className="footer__nav">
+                  <li
+                    className="footer__nav-item"
+                    key={popupTicketBuyText}
+                  >
+                    <button
+                      type="button"
+                      className="button footer__nav-link"
+                      onClick={handleTicketPopupToggle}
+                      data-testid="footer-tickets-popup-button"
+                    >
+                      {popupTicketBuyText}
+                    </button>
+                  </li>
+                  {footerUserLinks.map(({
+                    id,
+                    name,
+                    link,
+                  }) => (
+                    <li
+                      key={id}
+                      className="footer__nav-item"
+                    >
+                      <Link
+                        href={link}
+                        className="footer__nav-link"
+                      >
+                        {name}
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+              <div className="footer__col">
+                <h3 className="footer__title">{footerNavTitleRight}</h3>
+                <ul className="footer__nav">
+                  {footerAboutLinks.map(({
+                    id,
+                    name,
+                    link,
+                  }) => (
+                    <li
+                      key={id}
+                      className="footer__nav-item"
+                    >
+                      <Link
+                        href={link}
+                        className="footer__nav-link"
+                        onClick={(e) => {
+                          e.preventDefault();
+                          if (router.pathname !== link) {
+                            router.push(link);
+                          }
+                        }}
+                      >
+                        {name}
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+            <ul className="footer__contacts">
+              <li className="footer__contact">
+                <Link
+                  href={`tel:${phone}`}
+                  className="footer__contact-link"
                 >
-                  <button
-                    type="button"
-                    className="button footer__nav-link"
-                    onClick={handleTicketPopupToggle}
-                    data-testid="footer-tickets-popup-button"
-                  >
-                    {popupTicketBuyText}
-                  </button>
-                </li>
-                {footerUserLinks.map(({
-                  id,
-                  name,
-                  link,
-                }) => (
-                  <li
-                    key={id}
-                    className="footer__nav-item"
-                  >
-                    <Link
-                      href={link}
-                      className="footer__nav-link"
-                    >
-                      {name}
-                    </Link>
-                  </li>
-                ))}
-              </ul>
-            </div>
-            <div className="footer__col">
-              <h3 className="footer__title">{footerNavTitleRight}</h3>
-              <ul className="footer__nav">
-                {footerAboutLinks.map(({
-                  id,
-                  name,
-                  link,
-                }) => (
-                  <li
-                    key={id}
-                    className="footer__nav-item"
-                  >
-                    <Link
-                      href={link}
-                      className="footer__nav-link"
-                      onClick={(e) => {
-                        e.preventDefault();
-                        if (router.pathname !== link) {
-                          router.push(link);
-                        }
-                      }}
-                    >
-                      {name}
-                    </Link>
-                  </li>
-                ))}
-              </ul>
-            </div>
+                  {phone}
+                </Link>
+              </li>
+              <li className="footer__contact">
+                <Link
+                  href={`mailto:${email}`}
+                  className="footer__contact-link"
+                >
+                  {email}
+                </Link>
+              </li>
+            </ul>
           </div>
-          <ul className="footer__contacts">
-            <li className="footer__contact">
+          <div className="footer__middle">
+            {
+              isTablet && (
+                isDesktop
+                  ? (
+                    <div className="footer__official-name">
+                      <p className="footer__official-name-text">
+                        Муниципальное Бюджетное Учреждение Культуры «Зоопарк»
+                      </p>
+                    </div>
+                  )
+                  : (
+                    <div className="footer__official-name">
+                      <p className="footer__official-name-text">
+                        МБУК «Зоопарк»
+                      </p>
+                    </div>
+                  )
+              )
+            }
+            <div className="footer__social-media">
+              <SocialMedia
+                className="footer__social-icon"
+              />
+            </div>
+            <div className="footer__copyright">
+              Сайт разработан
               <Link
-                href={`tel:${phone}`}
-                className="footer__contact-link"
+                href="https://www.tourmalinecore.com/"
+                className="footer__copyright-link"
               >
-                {phone}
+                Tourmaline Core
+                <span className="footer__heart">❤</span>
               </Link>
-            </li>
-            <li className="footer__contact">
-              <Link
-                href={`mailto:${email}`}
-                className="footer__contact-link"
-              >
-                {email}
-              </Link>
-            </li>
-          </ul>
-        </div>
-        <div className="footer__middle">
-          {
-            isTablet && (
-              isDesktop
-                ? (
-                  <div className="footer__official-name">
-                    <p className="footer__official-name-text">
-                      Муниципальное Бюджетное Учреждение Культуры «Зоопарк»
-                    </p>
-                  </div>
-                )
-                : (
-                  <div className="footer__official-name">
-                    <p className="footer__official-name-text">
-                      МБУК «Зоопарк»
-                    </p>
-                  </div>
-                )
-            )
-          }
-          <div className="footer__social-media">
-            <SocialMedia
-              className="footer__social-icon"
-            />
-          </div>
-          <div className="footer__copyright">
-            Сайт разработан
-            <Link
-              href="https://www.tourmalinecore.com/"
-              className="footer__copyright-link"
-            >
-              Tourmaline Core
-              <span className="footer__heart">❤</span>
-            </Link>
+            </div>
           </div>
         </div>
       </div>
