f63e66e32ffe0720401c955a6d628471eeb374ee
Ekaterina <echekina@gmail.com>
2024-12-23 16:39:17 +0500

    fix: #88: fix type linting in pipeline

--- src/components/home-page/Tickets/Tickets.tsx
+++ src/components/home-page/Tickets/Tickets.tsx
@@ -76,6 +76,7 @@ export function Tickets({
                 key={el.id}
                 ticket={el}
                 isSubsidized
+                link=""
               />
             ))}
             {
