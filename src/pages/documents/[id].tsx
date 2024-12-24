5e62680aae4278a8f5968f1a90048bd8b00684e6
Artem Sheptunov <sheptunov-aa@mail.ru>
2024-12-24 08:55:21 +0500

    refactor: #105: change generating types and data retrieval

--- src/pages/documents/[id].tsx
+++ src/pages/documents/[id].tsx
@@ -1,6 +1,6 @@
+import { DocumentListResponse, DocumentsCategoryListResponse } from "@/src/common/api-types";
 import { DOCUMENTS_CATEGORIES, DocumentsCategoriesProps } from "@/src/common/mocks/documents-page-mock/documents-categories-mock";
 import { DOCUMENTS_LIST, DocumentsListComponentProps } from "@/src/common/mocks/documents-page-mock/documents-list-mock";
-import { Meta } from "@/src/common/types";
 import { api } from "@/src/common/utils/HttpClient";
 import { DocumentsList } from "@/src/components/documents-page/DocumentsList/DocumentsList";
 import { NotFound } from "@/src/components/not-found-page/NotFound/NotFound";
@@ -35,16 +35,6 @@ export default function DocumentsCategories({
   );
 }
 
-type CategoryResponse = {
-  data: DocumentsCategoriesProps[];
-  meta: Meta;
-};
-
-type DocumentsResponse = {
-  data: DocumentsListComponentProps[];
-  meta: Meta;
-};
-
 export async function getServerSideProps({
   query,
 }: {
@@ -75,15 +65,52 @@ export async function getServerSideProps({
 
   const documentsQueryParams = {
     populate: [`files`, `category`],
-  };
-
-  const category: CategoryResponse = await api.get(`/documents-categories?${qs.stringify(categoryQueryParams)}`);
-  const documents: DocumentsResponse = await api.get(`/documents?${qs.stringify(documentsQueryParams)}`);
-
-  return {
-    props: {
-      category: category.data[0].title,
-      documents: documents.data,
+    filters: {
+      category: {
+        id: {
+          $eq: query.id,
+        },
+      },
     },
   };
+
+  try {
+    const categoryResponse: DocumentsCategoryListResponse = await api.get(`/documents-categories?${qs.stringify(categoryQueryParams)}`);
+    const documentsResponse: DocumentListResponse = await api.get(`/documents?${qs.stringify(documentsQueryParams)}`);
+
+    const documents: DocumentsListComponentProps[] = documentsResponse.data!
+      .map((documentsItem) => ({
+        id: documentsItem.id!,
+        date: documentsItem.attributes!.publishedAt!,
+        showDate: documentsItem.attributes!.showDate!,
+        title: documentsItem.attributes!.title!,
+        subtitle: documentsItem.attributes!.subtitle,
+        description: documentsItem.attributes!.description,
+        files: documentsItem.attributes!.files!.data!.map((file) => ({
+          id: file.id!,
+          name: file.attributes!.name!,
+          url: file.attributes!.url!,
+          ext: file.attributes!.ext!,
+        })),
+        category: {
+          id: documentsItem.attributes!.category!.data!.id!,
+        },
+      }));
+
+    return {
+      props: {
+        category: {
+          id: categoryResponse.data![0].id,
+          title: categoryResponse.data![0].attributes!.title,
+        },
+        documents,
+      },
+    };
+  } catch {
+    return {
+      props: {
+        category: null,
+      },
+    };
+  }
 }
--- src/pages/documents/index.tsx
+++ src/pages/documents/index.tsx
@@ -2,9 +2,9 @@ import Head from 'next/head';
 import { NotFound } from '@/src/components/not-found-page/NotFound/NotFound';
 import { DOCUMENTS_PAGE, DocumentsPageProps } from '@/src/common/mocks/documents-page-mock/documents-page-mock';
 import { api } from '@/src/common/utils/HttpClient';
-import { Meta } from '@/src/common/types';
 import { DOCUMENTS_CATEGORIES, DocumentsCategoriesProps } from '@/src/common/mocks/documents-page-mock/documents-categories-mock';
 import { DocumentsCategories } from '@/src/components/documents-page/DocumentsCategories/DocumentsCategories';
+import { DocumentsCategoryListResponse } from '@/src/common/api-types';
 
 export default function DocumentsPage({
   pageData,
@@ -13,7 +13,7 @@ export default function DocumentsPage({
   pageData: DocumentsPageProps,
   documentCategories: DocumentsCategoriesProps[],
 }) {
-  if (!pageData) {
+  if (!pageData || !documentCategories) {
     return <NotFound />;
   }
 
@@ -39,27 +39,36 @@ export default function DocumentsPage({
   );
 }
 
-type DocumentsCategoriesResponse = {
-  data: DocumentsCategoriesProps[];
-  meta: Meta;
-};
-
 export async function getServerSideProps() {
   if (process.env.APP_ENV === `static`) {
     return {
       props: {
         pageData: DOCUMENTS_PAGE,
-        categories: DOCUMENTS_CATEGORIES,
+        documentCategories: DOCUMENTS_CATEGORIES,
       },
     };
   }
 
-  const documentsCategories: DocumentsCategoriesResponse = await api.get(`/documents-categories`);
+  try {
+    const documentsCategoriesResponse: DocumentsCategoryListResponse = await api.get(`/documents-categories`);
 
-  return {
-    props: {
-      pageData: DOCUMENTS_PAGE,
-      documentCategories: documentsCategories.data,
-    },
-  };
+    const documentsCategories: DocumentsCategoriesProps[] = documentsCategoriesResponse.data!
+      .map((documentsCategoriesItem) => ({
+        id: documentsCategoriesItem.id!,
+        title: documentsCategoriesItem.attributes!.title,
+      }));
+
+    return {
+      props: {
+        pageData: DOCUMENTS_PAGE,
+        documentCategories: documentsCategories,
+      },
+    };
+  } catch {
+    return {
+      props: {
+        documentCategories: null,
+      },
+    };
+  }
 }
