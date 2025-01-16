3733efc3d5b54db2875d1f5c948ab17c99e97ec8
Arina <arishakond@mail.ru>
2025-01-16 09:01:06 +0500

    feat: #28: add tabs to DocumentsList

--- src/components/documents-page/DocumentsList/DocumentsList.tsx
+++ src/components/documents-page/DocumentsList/DocumentsList.tsx
@@ -1,5 +1,8 @@
 import { DocumentsListComponentProps } from "@/src/common/mocks/documents-page-mock/documents-list-mock";
 import { DocumentCard } from "@/src/components/documents-page/DocumentsList/components/DocumentCard/DocumentCard";
+import { useState } from "react";
+import dayjs from "dayjs";
+import { Tabs } from "../../globals/Tabs/Tabs";
 
 export function DocumentsList({
   categoryTitle,
@@ -8,35 +11,49 @@ export function DocumentsList({
   categoryTitle: string,
   documents: DocumentsListComponentProps[],
 }) {
+  const [chosenYear, setChosenYear] = useState<number>(0);
+
+  console.log(documents?.filter(({
+    date,
+  }) => dayjs(date)
+    .year() === chosenYear));
+
   return (
     <section
       className="documents container"
     >
       <h1 className="documents__title">{categoryTitle}</h1>
+      <Tabs
+        setChosenYear={setChosenYear}
+      />
       <ul
         className="documents__list"
         data-testid="documents-list"
       >
-        {documents?.map(({
-          id,
+        {documents?.filter(({
           date,
-          showDate,
-          title,
-          subtitle,
-          description,
-          files,
-        }) => (
-          <DocumentCard
-            className="documents__item"
-            key={id}
-            date={date}
-            showDate={showDate}
-            title={title}
-            subtitle={subtitle}
-            description={description}
-            files={files}
-          />
-        ))}
+        }) => dayjs(date)
+          .year() === chosenYear)
+          .map(({
+            id,
+            date,
+            showDate,
+            title,
+            subtitle,
+            description,
+            files,
+          }) => (
+            <DocumentCard
+              className="documents__item"
+              key={id}
+              date={date}
+              showDate={showDate}
+              title={title}
+              subtitle={subtitle}
+              description={description}
+              files={files}
+            />
+          ))}
       </ul>
     </section>
   );
