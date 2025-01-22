821c16d084630cd2bcb4f0d50f0ee4a97f89e686
Dmitriy Myakotin <75628188+MDI74@users.noreply.github.com>
2025-01-22 15:32:27 +0500

    chore: #23: increased exprect timeout

--- playwright.config.ts
+++ playwright.config.ts
@@ -34,7 +34,7 @@ export default defineConfig({
   },
   expect: {
     // Maximum time expect() should wait for the condition to be met.
-    timeout: 10000,
+    timeout: 30000,
 
     toHaveScreenshot: {
       maxDiffPixels: 50,
