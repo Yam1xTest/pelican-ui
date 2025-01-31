f56068c042e554d9cbdf457a2c4d483ac4bfe1bf
sergiobagd <ssergioffx@gmail.com>
2025-01-31 17:22:27 +0500

    chore: adapt the test to new original axe-core plugin

--- playwright-tests/axe-tests/axeCheckUp.spec.tsx
+++ playwright-tests/axe-tests/axeCheckUp.spec.tsx
@@ -1,11 +1,9 @@
 import { test } from '@playwright/test';
-import AxePuppeteer from '@axe-core/playwright';
 import { writeFileSync, mkdirSync } from 'fs';
 import { dirname } from 'path';
 import { AppRoute, Breakpoint } from '@/src/common/enum';
-import { gotoPage } from '../helpers';
 
-test(`axeCheckUp`, async ({
+test(`axeCheckUp Desktop XL`, async ({
   page,
 }) => {
   await page.setViewportSize({
@@ -13,15 +11,14 @@ test(`axeCheckUp`, async ({
     height: 1080,
   });
 
-  await gotoPage({
-    page,
-    url: AppRoute.HOME,
+  await page.goto(AppRoute.HOME);
+
+  await page.addScriptTag({
+    path: require.resolve(`axe-core/axe.min.js`),
   });
 
-  const results = await new AxePuppeteer({
-    page,
-  } as any)
-    .analyze();
+  // Run axe-core analysis
+  const results = await page.evaluate(async () => await window.axe.run());
 
   const violationIdsToCheck = [
     `color-contrast`,
@@ -39,11 +36,9 @@ test(`axeCheckUp`, async ({
     `focusable-element`,
   ];
 
-  const violationsToCheck = results.violations.filter((violation) => violationIdsToCheck
-    .includes(violation.id));
+  const violationsToCheck = results.violations.filter((violation) => violationIdsToCheck.includes(violation.id));
 
   if (violationsToCheck.length > 0) {
-    // eslint-disable-next-line no-console
     console.table(violationsToCheck.map((violation) => ({
       id: violation.id,
       impact: violation.impact,
@@ -53,14 +48,122 @@ test(`axeCheckUp`, async ({
 
     throw new Error(`Accessibility violations found: ${violationsToCheck.length}`);
   } else {
-    // eslint-disable-next-line no-console
     console.log(`No accessibility violations found.`);
   }
 
-  const filePath = `./playwright-tests/axeReport/axe-report.json`;
+  const filePath = `./test/playwright-report/axeReport/axe-report.json`;
   mkdirSync(dirname(filePath), {
     recursive: true,
   });
 
   writeFileSync(filePath, JSON.stringify(violationsToCheck, null, 2));
+});
+
+test(`axeCheckUp Tablet XL`, async ({
+  page,
+}) => {
+  await page.setViewportSize({
+    width: Breakpoint.TABLET_XL,
+    height: 1080,
+  });
+
+  await page.goto(AppRoute.HOME);
+
+  await page.addScriptTag({
+    path: require.resolve(`axe-core/axe.min.js`),
+  });
+
+  const results = await page.evaluate(async () => await window.axe.run());
+
+  const violationIdsToCheck = [
+    `color-contrast`,
+    `area-alt`,
+    `image-alt`,
+    `image-redundant-alt`,
+    `input-image-alt`,
+    `duplicate-img-label`,
+    `non-empty-alt`,
+    `aria-label`,
+    `aria-labelledby`,
+    `color-contrast-enhanced`,
+    `focusable-no-name`,
+    `focusable-content`,
+    `focusable-element`,
+  ];
+
+  const violationsToCheck = results.violations.filter((violation) => violationIdsToCheck.includes(violation.id));
+
+  if (violationsToCheck.length > 0) {
+    console.table(violationsToCheck.map((violation) => ({
+      id: violation.id,
+      impact: violation.impact,
+      description: violation.description,
+      nodes: violation.nodes.length,
+    })));
+
+    throw new Error(`Accessibility violations found: ${violationsToCheck.length}`);
+  } else {
+    console.log(`No accessibility violations found.`);
+  }
+
+  const filePath = `./test/playwright-report/axeReport/axe-report.json`;
+  mkdirSync(dirname(filePath), {
+    recursive: true,
+  });
+
+  writeFileSync(filePath, JSON.stringify(violationsToCheck, null, 2));
+});
+
+test(`axeCheckUp Mobile`, async ({
+  page,
+}) => {
+  await page.setViewportSize({
+    width: Breakpoint.MOBILE,
+    height: 1080,
+  });
+
+  await page.goto(AppRoute.HOME);
+
+  await page.addScriptTag({
+    path: require.resolve(`axe-core/axe.min.js`),
+  });
+
+  const results = await page.evaluate(async () => await window.axe.run());
+
+  const violationIdsToCheck = [
+    `color-contrast`,
+    `area-alt`,
+    `image-alt`,
+    `image-redundant-alt`,
+    `input-image-alt`,
+    `duplicate-img-label`,
+    `non-empty-alt`,
+    `aria-label`,
+    `aria-labelledby`,
+    `color-contrast-enhanced`,
+    `focusable-no-name`,
+    `focusable-content`,
+    `focusable-element`,
+  ];
+
+  const violationsToCheck = results.violations.filter((violation) => violationIdsToCheck.includes(violation.id));
+
+  if (violationsToCheck.length > 0) {
+    console.table(violationsToCheck.map((violation) => ({
+      id: violation.id,
+      impact: violation.impact,
+      description: violation.description,
+      nodes: violation.nodes.length,
+    })));
+    throw new Error(`Accessibility violations found: ${violationsToCheck.length}`);
+  } else {
+    console.log(`No accessibility violations found.`);
+
+    const filePath = `./test/playwright-report/axeReport/axe-report.json`;
+    mkdirSync(dirname(filePath), {
+      recursive: true,
+    });
+
+    writeFileSync(filePath, JSON.stringify(violationsToCheck, null, 2));
+  }
 });
