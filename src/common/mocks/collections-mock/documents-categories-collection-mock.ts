818004b8fd5fff63fa57b0cf015bd9bd17d5f373
Arina <arishakond@mail.ru>
2025-01-31 16:33:23 +0500

    chore: #136: change DocumentsCategories mocks

--- src/common/mocks/collections-mock/documents-categories-collection-mock.ts
+++ src/common/mocks/collections-mock/documents-categories-collection-mock.ts
@@ -4,33 +4,41 @@ export const MOCK_DOCUMENTS_CATEGORIES: DocumentsCategoriesProps[] = [
   {
     id: 1,
     title: `Отчеты`,
+    isDivided: true,
   },
   {
     id: 2,
     title: `Торги`,
+    isDivided: true,
   },
   {
     id: 3,
     title: `Учредительные документы и\u00A0реквизиты`,
+    isDivided: false,
   },
   {
     id: 4,
     title: `Муниципальные задания и отчёты по\u00A0выполнению муниципального задания`,
+    isDivided: true,
   },
   {
     id: 5,
     title: `Закупки`,
+    isDivided: true,
   },
   {
     id: 6,
     title: `Приказы`,
+    isDivided: true,
   },
   {
     id: 7,
     title: `О деятельности учреждения`,
+    isDivided: true,
   },
   {
     id: 8,
     title: `Планы Финансово-Хозяйственной Деятельности`,
+    isDivided: true,
   },
 ];
