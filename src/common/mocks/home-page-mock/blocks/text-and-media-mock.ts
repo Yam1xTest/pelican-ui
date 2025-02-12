7c5f17e7f0eef1126acaea6d2ce64d87fa53ca15
Artem Sheptunov <sheptunov-aa@mail.ru>
2025-02-12 15:55:32 +0500

    test: #154: update snapshots and add new options to mocks

--- src/common/mocks/home-page-mock/blocks/text-and-media-mock.ts
+++ src/common/mocks/home-page-mock/blocks/text-and-media-mock.ts
@@ -6,9 +6,11 @@ export const MOCK_TEXT_AND_MEDIA: TextAndMediaComponentProps = {
   __component: BlockTypes.SHARED_TEXT_AND_MEDIA,
   title: `В зоопарке\n141 вид животных`,
   description: `Снежные барсы, ленивцы, росомахи, гепард и другие редкие животные, которые вас удивят.`,
-  video: {
-    alt: `Видео о зоопарке`,
+  media: {
+    alternativeText: `Видео о зоопарке`,
     url: `/video/text-and-media-video.mp4`,
     mime: `video/mp4`,
   },
+  contentOrder: `Текст слева, видео/изображение справа`,
+  viewFootsteps: true,
 };
