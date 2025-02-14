9c8e9c060a0eac04c6b5b552e53d311408edc627
Ekaterina <echekina@gmail.com>
2025-02-14 10:28:43 +0500

    chore: #161: remove redundant mock

--- src/common/mocks/internal-test-page-mock/internal-test-page-mock.ts
+++ src/common/mocks/internal-test-page-mock/internal-test-page-mock.ts
@@ -1,4 +1,4 @@
-import { MOCK_IMAGE_WITH_BUTTON_GRID } from "./blocks/image-with-button-grid-mock";
+import { MOCK_IMAGE_WITH_BUTTON_GRID } from "../home-page-mock/blocks/image-with-button-grid-mock";
 
 export const MOCK_INTERNAL_TEST_PAGE = {
   blocks: [MOCK_IMAGE_WITH_BUTTON_GRID],
