e8871c575159f0c262428e8e2aee5154204d96b8
Artem Sheptunov <sheptunov-aa@mail.ru>
2025-02-17 10:39:30 +0500

    test: #154: update test images and move TextAndMedia test to internal page

--- src/common/mocks/internal-test-page-mock/internal-test-page-mock.ts
+++ src/common/mocks/internal-test-page-mock/internal-test-page-mock.ts
@@ -1,6 +1,11 @@
 import { MOCK_IMAGE_WITH_BUTTON_GRID } from "../home-page-mock/blocks/image-with-button-grid-mock";
+import { MOCK_TEXT_AND_MEDIA } from "../home-page-mock/blocks/text-and-media-mock";
 import { MOCK_SHARED_CARDS } from "./blocks/shared-cards-mock";
 
 export const MOCK_INTERNAL_TEST_PAGE = {
-  blocks: [MOCK_SHARED_CARDS, MOCK_IMAGE_WITH_BUTTON_GRID],
+  blocks: [
+    MOCK_SHARED_CARDS,
+    MOCK_IMAGE_WITH_BUTTON_GRID,
+    MOCK_TEXT_AND_MEDIA,
+  ],
 };
