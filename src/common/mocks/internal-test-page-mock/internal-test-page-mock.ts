efe29f53dc03d104e3d834f1c7443ad9254261d2
Ekaterina <echekina@gmail.com>
2025-02-18 16:01:35 +0500

    chore: #171: minor fixes

--- src/common/mocks/internal-test-page-mock/internal-test-page-mock.ts
+++ src/common/mocks/internal-test-page-mock/internal-test-page-mock.ts
@@ -1,8 +1,8 @@
 import { MOCK_IMAGE_WITH_BUTTON_GRID } from "../home-page-mock/blocks/image-with-button-grid-mock";
 import { MOCK_TEXT_AND_MEDIA } from "../home-page-mock/blocks/text-and-media-mock";
-import { MOCK_SHARED_ARTICLE } from "./blocks/shared-article";
 import { MOCK_SHARED_CARDS } from "./blocks/shared-cards-mock";
 import { MOCK_SHARED_CATEGORIES } from "./blocks/shared-categories";
+import { MOCK_SHARED_ARTICLE } from "./blocks/shared-article";
 
 export const MOCK_INTERNAL_TEST_PAGE = {
   blocks: [
@@ -10,6 +10,6 @@ export const MOCK_INTERNAL_TEST_PAGE = {
     MOCK_IMAGE_WITH_BUTTON_GRID,
     MOCK_TEXT_AND_MEDIA,
     MOCK_SHARED_CATEGORIES,
-    MOCK_SHARED_ARTICLE
+    MOCK_SHARED_ARTICLE,
   ],
 };
