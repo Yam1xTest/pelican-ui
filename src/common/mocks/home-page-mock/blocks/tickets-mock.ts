967599eb0e8b49fc0ebc9a289a1f15fd82aa2a52
Artem Sheptunov <sheptunov-aa@mail.ru>
2025-02-24 09:07:29 +0500

    refactor: #173: update tickets mocks

--- src/common/mocks/home-page-mock/blocks/tickets-mock.ts
+++ src/common/mocks/home-page-mock/blocks/tickets-mock.ts
@@ -7,24 +7,28 @@ export const MOCK_TICKETS: TicketsComponentProps = {
   generalTicketsTitle: `Входные билеты`,
   generalTicketsLink: `https://widget.afisha.yandex.ru/w/sessions/ticketsteam-803@37605507?clientKey=3bc42fbd-a832-49aa-a269-79188e18d9e1&regionId=56`,
   subsidizedTicketsTitle: `Льготные билеты`,
-  subsidizedTicketsSubtitle: `Купить льготный билет можно только на кассе зоопарка.`,
+  subsidizedTicketsDescription: `Купить льготный билет можно только на кассе зоопарка.`,
+  subsidizedTicketsLink: `https://vk.com/topic-71671982_48253263`,
   generalTickets: [
     {
       id: 0,
-      category: `Взрослые, дети\u00A0от\u00A014\u00A0лет`,
+      category: `Взрослые,\nдети от 14 лет`,
       price: `400  ₽ / чел`,
+      theme: `Зелёный`,
     },
     {
       id: 1,
       category: `Дети от 5 до 13 лет`,
       description: `Требуется подтверждающий документ.`,
       price: `200  ₽ / чел`,
+      theme: `Зелёный`,
     },
     {
       id: 2,
       category: `Дети до 4 лет`,
       description: `Требуется подтверждающий документ.`,
       price: `Бесплатно`,
+      theme: `Зелёный`,
     },
   ],
   subsidizedTickets: [
@@ -34,18 +38,21 @@ export const MOCK_TICKETS: TicketsComponentProps = {
       description: `Требуется подтверждающий документ.`,
       price: `Бесплатно`,
       frequency: `1 раз в месяц`,
+      theme: `Зелёный`,
     },
     {
       id: 1,
       category: `Пенсионеры, студенты`,
       description: `Требуется подтверждающий документ.`,
       price: `200  ₽ / чел`,
+      theme: `Зелёный`,
     },
     {
       id: 2,
       category: `Инвалиды I и II групп`,
       description: `Требуется подтверждающий документ.`,
       price: `Бесплатно`,
+      theme: `Зелёный`,
     },
   ],
 };
