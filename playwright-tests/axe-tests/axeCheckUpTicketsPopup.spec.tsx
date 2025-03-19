85f260305011337c2159c27666612ce5cbf1b99a
Ekaterina <echekina@gmail.com>
2025-03-19 14:08:36 +0500

    test: #230: axe-core for tickets popup

---
+++ playwright-tests/axe-tests/axeCheckUpTicketsPopup.spec.tsx
@@ -0,0 +1,88 @@
+/* eslint-disable no-console */
+import { test } from '@playwright/test';
+import { AppRoute, Breakpoint } from '@/src/common/enum';
+import { axeCheckAndWriteReport, openTicketsPopupAccordions, setViewportSize } from '../helpers';
+
+test(`axeCheckUp Tickets Popup Desktop XL`, async ({
+  page,
+}) => {
+  await setViewportSize({
+    page,
+    width: Breakpoint.DESKTOP_XL,
+  });
+
+  await page.goto(AppRoute.HOME);
+
+  await page.getByTestId(`footer-tickets-popup-button`)
+    .click();
+
+  await openTicketsPopupAccordions({
+    page,
+  });
+
+  await page.addScriptTag({
+    path: require.resolve(`axe-core/axe.min.js`),
+  });
+
+  await axeCheckAndWriteReport({
+    page,
+    viewport: `desktop-xl`,
+    pageName: `tickets-popup`,
+  });
+});
+
+test(`axeCheckUp Tickets Popup Tablet XL`, async ({
+  page,
+}) => {
+  await setViewportSize({
+    page,
+    width: Breakpoint.TABLET_XL,
+  });
+
+  await page.goto(AppRoute.HOME);
+
+  await page.getByTestId(`footer-tickets-popup-button`)
+    .click();
+
+  await openTicketsPopupAccordions({
+    page,
+  });
+
+  await page.addScriptTag({
+    path: require.resolve(`axe-core/axe.min.js`),
+  });
+
+  await axeCheckAndWriteReport({
+    page,
+    viewport: `tablet-xl`,
+    pageName: `tickets-popup`,
+  });
+});
+
+test(`axeCheckUp Tickets Popup Mobile`, async ({
+  page,
+}) => {
+  await setViewportSize({
+    page,
+    width: Breakpoint.MOBILE,
+  });
+
+  await page.goto(AppRoute.HOME);
+
+  await page.getByTestId(`footer-tickets-popup-button`)
+    .click();
+
+  await openTicketsPopupAccordions({
+    page,
+  });
+
+  await page.addScriptTag({
+    path: require.resolve(`axe-core/axe.min.js`),
+  });
+
+  await axeCheckAndWriteReport({
+    page,
+    viewport: `mobile`,
+    pageName: `tickets-popup`,
+  });
+});
