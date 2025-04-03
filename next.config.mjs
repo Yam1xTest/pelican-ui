39079168a96fbf7ad823b69e2a710efb650445b8
Ekaterina <echekina@gmail.com>
2025-04-03 10:34:16 +0500

    feat: add CSP to next config

--- next.config.mjs
+++ next.config.mjs
@@ -24,7 +24,33 @@ const nextConfig = {
         hostname: '**'
       },
     ]
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: cspHeader.replace(/\n/g, ''),
+          },
+        ],
+      },
+    ]
   }
+
 };
+
+const cspHeader = `
+default-src 'self';
+script- src 'self' 'unsafe-eval' 'unsafe-inline';
+style - src 'self' 'unsafe-inline';
+img - src 'self' blob: data: ;
+font - src 'self';
+object - src 'none';
+base - uri 'self';
+form - action 'self';
+frame - ancestors 'none';
+upgrade - insecure - requests;`
 
 export default nextConfig;
