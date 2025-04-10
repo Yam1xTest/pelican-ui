98e62dd38bd92e259c5ee31bc2a4a92eeba45fe6
Ekaterina <echekina@gmail.com>
2025-04-10 08:07:48 +0500

    feat: add sample CORS headers to next config

--- next.config.mjs
+++ next.config.mjs
@@ -30,27 +30,40 @@ const nextConfig = {
       {
         source: '/(.*)',
         headers: [
-          {
-            key: 'Content-Security-Policy',
-            value: cspHeader.replace(/\n/g, ''),
-          },
+          // 1. CPS Headers:
+          // {
+          // key: 'Content-Security-Policy',
+          // value: cspHeader.replace(/\n/g, ''),
+          // },
+
+          // ----------------------------------------
+
+          // 2. CORS Headers
+          // By default, Next.js relies on a same-origin approach, imposing a strict policy. If you want to change that, you must configure it manually:
+
+          // { key: "Access-Control-Allow-Credentials", value: "true" },
+          // { key: "Access-Control-Allow-Origin", value: "*" }, // replace this with your actual origin
+          // { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+          // { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+
+          // This is to isolate browsing contexts like a tab or window to protect your site from XSS attacks and data stolen through shared browsing contexts.
+          // { key: "Cross-Origin-Opener-Policy", value: "same-origin" },
         ],
       },
     ]
   }
-
 };
 
-const cspHeader = `
-default-src 'self';
-script- src 'self' 'unsafe-eval' 'unsafe-inline';
-style - src 'self' 'unsafe-inline';
-img - src 'self' blob: data: ;
-font - src 'self';
-object - src 'none';
-base - uri 'self';
-form - action 'self';
-frame - ancestors 'none';
-upgrade - insecure - requests;`
+// const cspHeader = `
+// default-src 'self';
+// script- src 'self' 'unsafe-eval' 'unsafe-inline';
+// style - src 'self' 'unsafe-inline';
+// img - src 'self' blob: data: ;
+// font - src 'self';
+// object - src 'none';
+// base - uri 'self';
+// form - action 'self';
+// frame - ancestors 'none';
+// upgrade - insecure - requests;`
 
 export default nextConfig;
