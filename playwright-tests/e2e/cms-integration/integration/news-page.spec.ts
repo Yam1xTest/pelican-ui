fbdb793bed2791655e7d26af62bc1e4a5559272e
Dmitriy Myakotin <75628188+MDI74@users.noreply.github.com>
2025-04-16 11:38:48 +0500

    test: #287: refactoring news tests

--- playwright-tests/e2e/cms-integration/integration/news-page.spec.ts
+++ playwright-tests/e2e/cms-integration/integration/news-page.spec.ts
@@ -3,13 +3,24 @@ import { AppRoute } from "@/src/common/enum";
 import { getStrapiURL } from "@/src/common/utils/getStrapiURL";
 import test, { expect, Page } from "@playwright/test";
 import axios, { AxiosError, HttpStatusCode } from "axios";
-import { E2E_UI_NAME_PREFIX } from "../helpers/cms-integration-helpers";
+import { NewsCollection } from "@/src/common/api-types";
+import { E2E_UI_NAME_PREFIX, getFileIdByName } from "../helpers/cms-integration-helpers";
 
-const NEWS_TITLE = `${E2E_UI_NAME_PREFIX} Новости`;
+const NEWS_PAGE_TITLE = `${E2E_UI_NAME_PREFIX} Новости`;
+const NEWS_TITLE = `${E2E_UI_NAME_PREFIX} В зоопарке появился амурский тигр`;
+const DESCRIPTION = `На фотографии изображен амурский тигр!`;
+const INNER_CONTENT = `В зоопарке появился амурский тигр, приходите посмотреть!`;
+const NEWS_API_ENDPOINT = `${getStrapiURL()}/news`;
 const NEWS_PAGE_API_ENDPOINT = `${getStrapiURL()}/news-page`;
 
 test.describe(`News page CMS integration tests`, () => {
   test.beforeEach(async () => {
+    await cleanupTestNewsByTitle({
+      title: NEWS_TITLE,
+    });
+
+    await createTestNews();
+
     await updateTestNewsPage();
   });
 
@@ -21,8 +32,10 @@ test.describe(`News page CMS integration tests`, () => {
     `
       GIVEN news page without content
       WHEN call method PUT /api/news-page
+      AND call method POST /api/news
       AND go to news page
       SHOULD news page content is displayed correctly
+      AND news is displayed correctly
       `,
     checkNewsPageOnUiTest,
   );
@@ -40,13 +53,27 @@ async function checkNewsPageOnUiTest({
 
   expect(page.getByText(NEWS_TITLE), `News page title should be visible`)
     .toBeVisible();
+
+  expect(page.getByText(NEWS_TITLE), `News title should be visible`)
+    .toBeVisible();
+
+  expect(page.getByText(DESCRIPTION), `News description should be visible`)
+    .toBeVisible();
+
+  await page.getByText(NEWS_TITLE)
+    .click();
+
+  await page.waitForURL(`${AppRoute.NEWS}/**`);
+
+  expect(page.getByText(INNER_CONTENT), `News content should be visible`)
+    .toBeVisible();
 }
 
 async function updateTestNewsPage() {
   try {
     const response = await axios.put(NEWS_PAGE_API_ENDPOINT, {
       data: {
-        title: NEWS_TITLE,
+        title: NEWS_PAGE_TITLE,
       },
     });
 
@@ -65,5 +92,44 @@ async function cleanupTestNewsPage() {
       .toEqual(HttpStatusCode.NoContent);
   } catch (error) {
     throw new Error(`Failed to delete test news page: ${(error as AxiosError).message}`);
+  }
+}
+
+async function createTestNews() {
+  try {
+    const response = await axios.post(NEWS_API_ENDPOINT, {
+      data: {
+        title: NEWS_TITLE,
+        description: DESCRIPTION,
+        image: await getFileIdByName(),
+        innerContent: INNER_CONTENT,
+      },
+    });
+
+    await expect(response.status, `News should be created with status 201`)
+      .toEqual(HttpStatusCode.Created);
+  } catch (error) {
+    throw new Error(`Failed to create test news: ${(error as AxiosError).message}`);
+  }
+}
+
+async function cleanupTestNewsByTitle({
+  title,
+}: {
+  title: string;
+}) {
+  try {
+    const newsResponse = (await axios.get(`${NEWS_API_ENDPOINT}?populate=*`)).data;
+
+    const testNews = newsResponse.data.find((item: NewsCollection) => item.title === title);
+
+    if (testNews) {
+      const response = await axios.delete(`${NEWS_API_ENDPOINT}/${testNews.documentId}`);
+
+      await expect(response.status, `News should be deleted with status 204`)
+        .toEqual(HttpStatusCode.NoContent);
+    }
+  } catch (error) {
+    throw new Error(`Failed to delete test news: ${(error as AxiosError).message}`);
   }
 }
