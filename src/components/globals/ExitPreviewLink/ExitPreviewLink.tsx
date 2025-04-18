6e057a1f23b9c129b261a8931e1b0f0fe130b93f
Dmitriy Myakotin <75628188+MDI74@users.noreply.github.com>
2025-04-18 16:01:29 +0500

    fix: fix lint

--- src/components/globals/ExitPreviewLink/ExitPreviewLink.tsx
+++ src/components/globals/ExitPreviewLink/ExitPreviewLink.tsx
@@ -2,18 +2,18 @@ import axios from "axios";
 import { useRouter } from "next/router";
 
 export function ExitPreviewLink() {
-  const router = useRouter()
-  
+  const router = useRouter();
+
   const handleExit = async () => {
-      await axios.post('/api/exit-preview');
-      router.reload();
+    await axios.post(`/api/exit-preview`);
+    router.reload();
   };
-  
+
   return (
     <button
+      type="button"
       className="button button--secondary exit-preview-link"
       onClick={handleExit}
-
     >
       Выйти из режима черновика
     </button>
