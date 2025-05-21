97fdd531d149c5fb7fdd8381e5ca524774008d05
fpandyz <5389368+fpandyz@users.noreply.github.com>
2025-05-21 09:15:24 +0500

    docs: add comment to src/pages/api/health-check.ts

--- src/pages/api/health-check.ts
+++ src/pages/api/health-check.ts
@@ -1,5 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
+// endpoint is /api/health-check
 export default function check(req: NextApiRequest, res: NextApiResponse) {
   return res.status(200)
     .json({
