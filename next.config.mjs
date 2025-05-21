17c10b4d4c3227ae54401ee9d09e9f00b365b15f
akovylyaeva <akovylyaeva@tourmalinecore.com>
2025-05-21 10:43:26 +0500

    chore: add BASE_URL env

--- next.config.mjs
+++ next.config.mjs
@@ -18,7 +18,7 @@ const nextConfig = {
           // Specifies the origin that has access to the resource
           {
             key: "Access-Control-Allow-Origin",
-            value: "https://chelzoo.tech",
+            value: process.env.BASE_URL,
           },
 
           // Indicates how the browser should handle opening new windows and tabs in the context of cross-origin requests
