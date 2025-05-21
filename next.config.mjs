d24ee057b5cb0efe51838aa053b993d51dc711f9
Dmitriy Myakotin <75628188+MDI74@users.noreply.github.com>
2025-05-21 14:27:52 +0500

    chore: rename ENV to disable image optimization

--- next.config.mjs
+++ next.config.mjs
@@ -151,7 +151,7 @@ const nextConfig = {
       path: `https://${process.env.CDN_DOMAIN}/_next/image`
     }),
 
-    unoptimized: process.env.NODE_ENV === 'test',
+    unoptimized: process.env.IMAGE_OPTIMIZATION_DISABLED === 'true',
 
     // lifetime in seconds for cached optimized images
     // https://nextjs.org/docs/pages/api-reference/components/image#minimumcachettl
