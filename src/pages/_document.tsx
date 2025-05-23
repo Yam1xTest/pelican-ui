ba9e5c01d785e3ee7a558c7781480c62c066c315
Ekaterina <echekina@gmail.com>
2025-05-23 14:09:16 +0500

    chore: move favicon to root

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -59,13 +59,13 @@ class AppDocument extends Document {
           <link
             rel="icon"
             type="image/png"
-            href="/favicon/favicon-96x96.png"
+            href="/favicon-96x96.png"
             sizes="96x96"
           />
           <link
             rel="icon"
             type="image/svg+xml"
-            href="/favicon/favicon.svg"
+            href="/favicon.svg"
           />
           <link
             rel="shortcut icon"
@@ -74,11 +74,11 @@ class AppDocument extends Document {
           <link
             rel="apple-touch-icon"
             sizes="180x180"
-            href="/favicon/apple-touch-icon.png"
+            href="/apple-touch-icon.png"
           />
           <link
             rel="manifest"
-            href="/favicon/site.webmanifest"
+            href="/site.webmanifest"
           />
         </Head>
 
