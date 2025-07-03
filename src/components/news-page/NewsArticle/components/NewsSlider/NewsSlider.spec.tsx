b04dfb3fd9d228b403a3b9ed2d91da29c93d1123
Dmitriy Myakotin <75628188+MDI74@users.noreply.github.com>
2025-07-03 16:30:00 +0500

    refactor: refactoring NewsSlider.spec.tsx

--- src/components/news-page/NewsArticle/components/NewsSlider/NewsSlider.spec.tsx
+++ src/components/news-page/NewsArticle/components/NewsSlider/NewsSlider.spec.tsx
@@ -1,17 +1,12 @@
+import { BREAKPOINTS } from '@/playwright-tests/constants/breakpoints';
 import {
-  CustomTestFixtures,
   expect,
   Page,
-  test,
+  test
 } from '@/playwright-tests/custom-test';
 import {
-  AppRoute,
-  Breakpoint,
-  BreakpointName,
-  ComponentName,
+  AppRoute, ComponentName
 } from '@/src/common/enum';
-
-const TEST_ID = `news-slider`;
 
 test.describe(`NewsSliderComponentTest`, () => {
   test.beforeEach(async ({
@@ -22,15 +17,21 @@ test.describe(`NewsSliderComponentTest`, () => {
 
   test(`NavigationTest`, navigationTest);
 
-  test(`MobileTest`, mobileTest);
-
-  test(`TabletTest`, tabletTest);
-
-  test(`TabletXlTest`, tabletXlTest);
-
-  test(`DesktopTest`, desktopTest);
-
-  test(`DesktopXlTest`, desktopXlTest);
+  for (const {
+    name,
+    breakpoint,
+    breakpointName,
+  } of BREAKPOINTS) {
+    test(name, async ({
+      testScreenshotAtBreakpoint,
+    }) => {
+      await testScreenshotAtBreakpoint({
+        testId: `news-slider`,
+        breakpoint,
+        breakpointName,
+      });
+    });
+  }
 });
 
 async function navigationTest({
@@ -44,95 +45,4 @@ async function navigationTest({
 
   await expect(page)
     .toHaveURL(`${AppRoute.NEWS}/2024/09/29/amurskii-tigr-i-kakie-u-nego-yest-problemi`);
-}
-
-async function mobileTest({
-  page,
-  setViewportSize,
-}: {
-  page: Page;
-  setViewportSize: CustomTestFixtures['setViewportSize'];
-}) {
-  await setViewportSize();
-
-  await expect(getNewsSliderByTestId({
-    page,
-  }))
-    .toHaveScreenshot(`${TEST_ID}-${BreakpointName.MOBILE}.png`);
-}
-
-async function tabletTest({
-  page,
-  setViewportSize,
-}: {
-  page: Page;
-  setViewportSize: CustomTestFixtures['setViewportSize'];
-}) {
-  await setViewportSize({
-    width: Breakpoint.TABLET,
-  });
-
-  await expect(getNewsSliderByTestId({
-    page,
-  }))
-    .toHaveScreenshot(`${TEST_ID}-${BreakpointName.TABLET}.png`);
-}
-
-async function tabletXlTest({
-  page,
-  setViewportSize,
-}: {
-  page: Page;
-  setViewportSize: CustomTestFixtures['setViewportSize'];
-}) {
-  await setViewportSize({
-    width: Breakpoint.TABLET_XL,
-  });
-
-  await expect(getNewsSliderByTestId({
-    page,
-  }))
-    .toHaveScreenshot(`${TEST_ID}-${BreakpointName.TABLET_XL}.png`);
-}
-
-async function desktopTest({
-  page,
-  setViewportSize,
-}: {
-  page: Page;
-  setViewportSize: CustomTestFixtures['setViewportSize'];
-}) {
-  await setViewportSize({
-    width: Breakpoint.DESKTOP,
-  });
-
-  await expect(getNewsSliderByTestId({
-    page,
-  }))
-    .toHaveScreenshot(`${TEST_ID}-${BreakpointName.DESKTOP}.png`);
-}
-
-async function desktopXlTest({
-  page,
-  setViewportSize,
-}: {
-  page: Page;
-  setViewportSize: CustomTestFixtures['setViewportSize'];
-}) {
-  await setViewportSize({
-    width: Breakpoint.DESKTOP_XL,
-  });
-
-  await expect(getNewsSliderByTestId({
-    page,
-  }))
-    .toHaveScreenshot(`${TEST_ID}-${BreakpointName.DESKTOP_XL}.png`);
-}
-
-function getNewsSliderByTestId({
-  page,
-}: {
-  page: Page;
-}) {
-  return page.getByTestId(TEST_ID);
 }
