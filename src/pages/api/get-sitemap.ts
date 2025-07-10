71a2d026158c80a74baec234732e0c96791a7ba7
Dmitriy Myakotin <75628188+MDI74@users.noreply.github.com>
2025-07-10 11:08:12 +0500

    chore(get-sitemap): update comments

--- src/pages/api/get-sitemap.ts
+++ src/pages/api/get-sitemap.ts
@@ -1,7 +1,7 @@
 import { apiFetch } from '@/src/common/utils/HttpClient';
 import { NextApiRequest, NextApiResponse } from 'next';
 
-// Endpoint for getting sitemap.xml from Strapi
+// It is needed in order to secretly trigger an strapi endpoint.
 export default async function getSitemap(_: NextApiRequest, res: NextApiResponse) {
   const xmlData = await apiFetch(`/strapi-5-sitemap-plugin/sitemap.xml`, {
     isResponseText: true,
